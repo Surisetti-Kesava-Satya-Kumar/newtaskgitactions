19ae8f9519ba86380ebe271ebb1ad16d0dd17538
Satyakumar Surisetti <satyakumar_surisetti@epam.com>
2023-02-02 15:28:38 +0530

    qwert

--- main.tf
+++ main.tf
@@ -17,15 +17,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_key_pair" "primeStore-key-pair" {
-  key_name   = "satya"
-  public_key = ${{ secrets.TF_PUBLIC_KEY }}
-}
-
 resource "aws_instance" "demo" {
   ami           = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.primeStore-key-pair.id
   tags = {
     Name = "Demo"
   }
